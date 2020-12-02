5fcaaa6e18f479318493788252dfef3a9ad795cd
李满满 <limanman@corp.netease.com>
2020-12-03 00:21:53 +0800

    :underage:

---
+++ namekox_prometheus/cli/subctx/prometheus.py
@@ -0,0 +1,16 @@
+#! -*- coding: utf-8 -*-
+
+# author: forcemain@163.com
+
+
+from namekox_prometheus.core.proxy import PrometheusProxy
+
+
+class Prometheus(object):
+    def __init__(self, config):
+        self.config = config
+        self.proxy = PrometheusProxy(config)
+
+    @classmethod
+    def name(cls):
+        return 'prometheus'
---
+++ namekox_prometheus/constants.py
@@ -0,0 +1,6 @@
+#! -*- coding: utf-8 -*-
+
+# author: forcemain@163.com
+
+
+PROMETHEUS_CONFIG_KEY = 'PROMETHEUS'

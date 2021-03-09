cbf694178d27dcecfc3bf14b30d4fc2e699be89b
李满满 <limanman@corp.netease.com>
2021-03-09 11:45:54 +0800

    :underage:

--- setup.py
+++ setup.py
@@ -16,14 +16,14 @@ readme = open(os.path.join(curdir, 'READIT.md'), 'rb', 'utf-8').read()
 
 setup(
     name='namekox-prometheus',
-    version='0.0.3',
+    version='0.0.4',
     description='namekox prometheus',
     long_description=readme,
     long_description_content_type='text/markdown',
     author='forcemain',
     url='https://forcemain.github.io/',
     packages=find_packages(exclude=['test', 'test.*']),
-    install_requires=["namekox-core>=0.0.1", "prometheus-client>=0.9.0"],
+    install_requires=["namekox-core", "prometheus-client==0.9.0"],
     zip_safe=True,
     license='Apache License, Version 2.0',
     classifiers=[
